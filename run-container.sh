e6cb1314daabb83d064c2dd9d134bebc3e8c2a6f
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2021-11-22 08:55:54 +0100

    run-container.sh: add from nrf-hal repo
    This file was initially commited here:
    https://github.com/fobnail/nrf-hal/blob/blinky-demo-nrf52840/run-container.sh
    
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ run-container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+docker run --privileged \
+    --rm -it \
+    -v $dir:/home/build/nrf-hal \
+    -v /dev:/dev \
+    -w /home/build/nrf-hal \
+    3mdeb/fobnail-sdk /bin/bash
